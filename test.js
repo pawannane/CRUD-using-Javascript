f49df33c0a6c107cca08d9b1e7f641b0e2f468c3
Pawan Nane <56354232+pawannane@users.noreply.github.com>
2022-07-15 00:37:37 +0530

    javascript quiz question test

---
+++ test.js
@@ -0,0 +1,15 @@
+let obj = { x: 0, y: 0, z: 2 };
+// Write below the command which will give the following output ['x', 'y', 'z']
+
+// console.log(Object.keys(obj));
+
+// let kim = "Kim"
+// kim.age = 88;
+// console.log(kim.age);
+
+let arr = [];
+arr.push(1, 2, 3, 4, 5)
+// console.log(arr);
+
+let x = [1, 2, 3]; 
+console.log(...x);
